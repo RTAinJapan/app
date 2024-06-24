6a58560bf9cb01339608eda302fcfbf01a1f23b4
Keiichiro Amemiya <hoishinxii@gmail.com>
2024-06-25 03:55:21 +0900

    db not null

--- projects/db-schema/migrations/0000_init.sql
+++ projects/db-schema/migrations/0000_init.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `users` (
 	`id` text PRIMARY KEY NOT NULL,
-	`discord_id` text,
-	`display_name` text,
-	`created_at` integer,
-	`updated_at` integer
+	`discord_id` text NOT NULL,
+	`display_name` text NOT NULL,
+	`created_at` integer NOT NULL,
+	`updated_at` integer NOT NULL
 );
 --> statement-breakpoint
 CREATE UNIQUE INDEX `users_discord_id_unique` ON `users` (`discord_id`);
